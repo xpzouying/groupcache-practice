57b9f696526f98572a0511dc9233f1247a5812f8
Ying Zou <xpzouying@gmail.com>
2017-10-17 13:42:29 +0800

    feature: add groupcache example support

--- README.md
+++ README.md
@@ -8,8 +8,50 @@ Environments:
 - based on custom slow database for test
 - multiple cache nodes for HA
 
-todo list
 
-- [ ] write custom database for test
-- [ ] write cache server to provide API for caller outside
+TODO list:
+
+- [x] write custom database for test
+- [x] write cache server to provide API for caller outside
 - [ ] write outside tester
+
+
+**Run step:**
+
+1. run database server
+
+```bash
+cd database && go run main.go
+```
+
+2. insert data into database
+
+```bash
+# insert one entry:
+# key: name, value: zouying
+curl -H "Content-Type: application/json" -X POST -d '{"key": "name", "value": "zouying"}' http:localhost:9000/set
+
+# check insert
+curl -H "Content-Type: application/json" -X POST -d '{"key": "name"}' http:localhost:9000/get
+```
+
+3. run frontend (include cache). Two node in cache cluster.
+
+```bash
+cd frontend
+
+# run the first node
+go run ./main.go -addr=":8001" -port ":18001"
+
+# run the second node
+go run ./main.go -addr=":8002" -port ":18002"
+```
+
+4. get the value, the first try will get from database
+```bash
+# try the first node api
+curl -H "Content-Type: application/json" -X POST -d '{"key": "name"}' http:localhost:18001/get
+
+# try the second node api
+curl -H "Content-Type: application/json" -X POST -d '{"key": "name"}' http:localhost:18002/get
+```
---
+++ frontend/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"net/http"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/golang/groupcache"
+)
+
+var (
+	cache *groupcache.Group
+)
+
+type request struct {
+	Key   string `json:"key"`
+	Value string `json:"value,omitempty"`
+}
+
+// WriteJSON write json format for response
+func WriteJSON(w http.ResponseWriter, code int, v interface{}) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	return json.NewEncoder(w).Encode(v)
+}
+
+// newResponse makes a new response
+// res: succ, or fail
+// msg: error msg or success return value
+func newResponse(res, msg string) map[string]string {
+	return map[string]string{"result": res, "message": msg}
+}
+
+// Get return result
+func Get(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		WriteJSON(w, http.StatusMethodNotAllowed, newResponse("fail", "bad request"))
+		return
+	}
+	body, _ := ioutil.ReadAll(r.Body)
+	var req request
+	if err := json.Unmarshal(body, &req); err != nil {
+		WriteJSON(w, http.StatusBadRequest, newResponse("fail", err.Error()))
+		return
+	}
+
+	var ctx groupcache.Context
+	var data []byte
+	if err := cache.Get(ctx, req.Key, groupcache.AllocatingByteSliceSink(&data)); err != nil {
+		WriteJSON(w, http.StatusBadRequest, newResponse("fail", err.Error()))
+		return
+	}
+
+	log.Infof("get %s's value: %s", req.Key, data)
+	WriteJSON(w, http.StatusOK, newResponse("succ", string(data)))
+}
+
+func main() {
+	addr := flag.String("addr", ":8001", "local address")
+	port := flag.String("port", ":18001", "listen port")
+	flag.Parse()
+
+	peers := groupcache.NewHTTPPool("http://localhost" + *addr)
+	peers.Set("http://localhost:8001", "http://localhost:8002")
+
+	// init cache
+	cache = groupcache.NewGroup("database_cache", 64<<20, groupcache.GetterFunc(cacheGetFunc))
+
+	go http.ListenAndServe(*addr, http.HandlerFunc(peers.ServeHTTP))
+
+	log.Infof("listen port: %v", *port)
+	http.HandleFunc("/get", Get)
+	log.Panic(http.ListenAndServe(*port, nil))
+}
+
+func cacheGetFunc(ctx groupcache.Context, key string, dest groupcache.Sink) error {
+	log.Infof("asking for key: %s", key)
+	req := request{Key: key}
+	data, err := json.Marshal(req)
+	if err != nil {
+		panic(err)
+	}
+	resp, err := http.Post("http://localhost:9000/get", "application/json", bytes.NewReader(data))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	data, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("read response body error: %v", err)
+		return err
+	}
+	log.Infof("read db response data: %s", data)
+
+	if resp.StatusCode != http.StatusOK {
+		dest.SetBytes([]byte("error http status"))
+		return nil
+	}
+
+	dest.SetBytes(data)
+	return nil
+}
